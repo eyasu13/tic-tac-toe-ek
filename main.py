64588faeee553c7f7bcb4a885cbdc90821e85110
Eyasuk <53485615+Eyasuk@users.noreply.github.com>
2020-05-17 19:06:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 def displayBoard(boardList):
     print("-----------")
-    print("| "+boardList[9]+" | "+boardList[8]+" | "+boardList[7]+" |")
+    print("| "+boardList[7]+" | "+boardList[8]+" | "+boardList[9]+" |")
     print("-------------")
     print("| "+boardList[4]+" | "+boardList[5]+" | "+boardList[6]+" |")
     print("-------------")
@@ -29,6 +29,13 @@ def checkWinner(boardList,letter):
 def checkFreeSpace(boardList,move):
     return boardList[move]==""
 
+def getBoardCopy(board):
+    dupeBoard=[]
+
+    for i in board:
+        dupeBoard.append(i)
+
+    return dupeBoard
 def checkdraw(boardList):
     for i in range(1,10):
         if checkFreeSpace(boardList,i):
@@ -37,18 +44,33 @@ def checkdraw(boardList):
 
 def computerMove(boardList,cL,pL):
     for i in range(1,10):
-        copyBoard=boardList
+        copyBoard=getBoardCopy(boardList)
         if checkFreeSpace(copyBoard,i):
             copyBoard[i]=cL
             if checkWinner(copyBoard,cL):
                 return i
     for i in range(1,10):
-        copyBoard=boardList
+        copyBoard=getBoardCopy(boardList)
         if checkFreeSpace(copyBoard,i):
             copyBoard[i]=pL
             if checkWinner(copyBoard,pL):
                 return i
-    return 8
+    tList=[]
+    found=0
+    for i in [1,3,7,9]:
+        copyBoard=getBoardCopy(boardList)
+        if copyBoard[i]=="":
+            tList.append(i)
+            found=1
+    if found==1:
+        return random.choice(tList)
+    if boardList[5]=="":
+        return 5
+    for i in [2,4,6,8]:
+        copyBoard=getBoardCopy(boardList)
+        if copyBoard[i]=="":
+            tList.append(i)
+    return random.choice(tList)   
 
 
 def firstMove():
@@ -59,7 +81,9 @@ def playerChoice():
         return ["X","O"]
     else:
         return ["O","X"]
+
 print("------------ Tic Tac Toe ------------")
+youS=computerS=d=0
 while True:
     board=[""]*10
     gameStatus=True
@@ -71,32 +95,44 @@ while True:
     else:
         turn="computer"
     print("'{}' goes First".format(turn))
-    while gameStatus:        
+    while gameStatus:
+        
         if turn=="player":
             displayBoard(board)
             y=playerMove(board)
             board[y]=playerLetter
             if checkWinner(board,playerLetter):
+                displayBoard(board)
                 print("You win!!!!")
+                youS=youS+1
                 gameStatus=False
             if checkdraw(board)==True:
+                displayBoard(board)
+                d=d+1
                 print("-----Draw-----")
                 gameStatus=False
             turn="computer"
         else:
+            
             x=computerMove(board,computerLetter,playerLetter)
             board[x]=computerLetter
-            displayBoard(board)
             if checkWinner(board,computerLetter):
+                displayBoard(board)
                 print("computer win!!!!")
+                computerS=computerS+1
                 gameStatus=False
             elif checkdraw(board):
+                displayBoard(board)
+                d=d+1
                 print("-----Draw-----")
                 gameStatus=False
             else:
                 turn="player"
-
-            
-    
+    if not gameStatus:
+        if input("do you like to play again 'y' or 'n'")=="y":
+            continue
+    else:
+        print("computer: "+str(computerS)+" draw: "+str(d)+" you: "+str(youS))
+        break
 
     
